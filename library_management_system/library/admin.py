8f522cda0eca0e25e839731c79bbfbf65691eb62
Huda Abdela <hudaabdulah75@gmail.com>
2025-01-10 23:31:24 +0300

    applied migrations and register models in the Django admin interface.

--- library_management_system/library/admin.py
+++ library_management_system/library/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, UserProfile, Borrow
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(UserProfile)
+admin.site.register(Borrow)
